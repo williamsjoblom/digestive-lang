5997db94fb5a76e61f0f5dcb2c8a440efd68530e
William Sjoblom <wllmsjblm@gmail.com>
2017-02-08 16:17:30 +0100

    Added script for a neat ipc interface to the running compiler

---
+++ dci.py
@@ -0,0 +1,24 @@
+
+import sys, os
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = os.getcwd()
+
+path = os.path.join(path, 'in')
+    
+if not os.path.exists(path):
+    print("'" + path + "' file not found")
+    sys.exit(1)
+
+out = open(path, 'w')
+    
+while True:
+    cmd = ""
+    if sys.version
+    cmd = input('> ')
+    print(cmd)
+    out.write(cmd)
+
+
