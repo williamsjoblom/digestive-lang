25a272e684bd15b470da893409e2ec64f515b317
williamsjoblom <wllmsjblm@gmail.com>
2017-12-19 14:27:08 +0100

    Add dir-locals for cmake-ide.el

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((nil
+  (cmake-ide-build-dir . "/home/wax/workbench/compiler-experiment/build/")))
+
+
+
+;; ((nil . ((eval . (set (make-local-variable 'my-project-path)
+;;                       (file-name-directory
+;;                        (let ((d (dir-locals-find-file ".")))
+;;                          (if (stringp d) d (car d))))))
+;;          (cmake-ide-project-dir . my-project-path)
+;;          (eval . (setq cmake-ide-build-dir (concat my-project-path "build")))
+;;          )))
