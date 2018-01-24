dbec106b76b489373efffa95fc9b04e60fdd992c
williamsjoblom <pijm0development@gmail.com>
2018-01-24 18:24:33 +0100

    Fix recursive calls not resolving correctly

--- genir/Function.cpp
+++ genir/Function.cpp
@@ -2,11 +2,10 @@
 #include "Function.h"
 
 void Generate::function(TACFun* fun, FunctionDecl* decl) {
-    decl->body->generate(fun);
-
-    fun->add(TACC::ret, fun->newImm<int>(0), TACOp(), TACOp());
-
     decl->irId = fun->id;
+    
+    decl->body->generate(fun);
+    fun->add(TACC::ret, fun->newImm<int>(0), TACOp(), TACOp());
 
     fun->dumpAssembly = decl->dumpAssembly;
     if (decl->dumpIr) fun->dump();
--- genir/Program.cpp
+++ genir/Program.cpp
@@ -5,9 +5,8 @@ void Generate::unit(TACProgram& program, Unit* unit) {
     int funId = 1;
     for (FunctionDecl* decl : unit->functions) {
 	TACFun* funcEnv = new TACFun(&program, funId++, decl);
-	decl->generate(funcEnv);
-
 	program.functions.push_back(funcEnv);
+	decl->generate(funcEnv);
     }
 
     TACFun* entryFun = new TACFun(&program, 0);
